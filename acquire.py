8d6d293efacfc2544c9868afd776f6e568040ef0
Neal Sheehan <championeal@gmail.com>
2020-06-04 22:24:24 -0500

    Streamlined class functions
    Added base draw functions in the Tile class. Also added board highlight on player tile hover.

--- acquire.py
+++ acquire.py
@@ -5,6 +5,7 @@ import random
 
 # file directory
 path = os.path.dirname(__file__)
+font_file = os.path.join(path, "TarzanaNarrow_Bold.ttf")
 
 # set window position
 #os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (50,50)
@@ -13,9 +14,16 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'
 #configuration
 config =\
 {
- 'window_width': 1280,
- 'window_height': 720,
- 'fps': 60
+'window_width': 1280,
+'window_height': 720,
+'fps': 30
+}
+
+colors =\
+{
+'gry_tile': (125,125,125),
+'blu_outline': (0,0,125),
+'blk_background': (30,30,30)
 }
 
 def center(background_size, object_size):
@@ -33,9 +41,9 @@ class Acquire(object):
 
 		pygame.init()
 		pygame.display.set_caption("Acquire")
-		flags = pygame.DOUBLEBUF
+		#flags = pygame.DOUBLEBUF
 		self.window = pygame.display.set_mode(
-									(self.window_width, self.window_height), flags)
+									(self.window_width, self.window_height))
 		self.background = pygame.Surface(self.window.get_size()).convert()
 		self.clock = pygame.time.Clock()
 		self.playtime = 0.0
@@ -48,10 +56,11 @@ class Acquire(object):
 			self.pile_tiles.append(f"{num}{letter}")
 		print(self.pile_tiles)
 
-		self.board = Board(self.background)
+		self.board = Board((self.rows, self.cols), self.background)
 		self.player = Player(self.max_player_tiles)
-		self.pile_tiles, self.player_tiles = self.player.select_tiles(self.pile_tiles)
-		self.player.draw_player_tiles(self.board, self.background)
+		self.player.starting_tiles(self.pile_tiles, self.board.rack_x,
+								self.board.rack_y, self.board.tile_size)
+		self.board.draw_player_tiles(self.player.my_tiles)
 
 	def run(self):
 		"""The mainloop"""
@@ -67,12 +76,20 @@ class Acquire(object):
 				elif event.type == pygame.MOUSEBUTTONDOWN:
 					self.on_mouse_down(event)
 
+			for label, tile in self.player.my_tiles.items():
+				if tile.rect.collidepoint(pygame.mouse.get_pos()):
+					if tile.hover == False:
+						tile.hover = True
+						self.board.highlight_tile(label)
+				else:
+					tile.hover = False
+					self.board.unhighlight_tile(label)
 
 
 			milliseconds = self.clock.tick(self.fps)
 			self.playtime += milliseconds / 1000.0
 			self.draw_text(f"FPS: {round(self.clock.get_fps())}")
-			pygame.display.flip()
+			pygame.display.update()
 
 		pygame.quit()
 
@@ -86,178 +103,147 @@ class Acquire(object):
 	def on_mouse_down(self, mouse):
 		print(mouse.pos, mouse.button)
 		if mouse.button == 1:
-			for tile in self.player_tiles:
+			for tile in self.player.my_tiles.values():
 				if tile.rect.collidepoint(pygame.mouse.get_pos()):
-					print("collide")
-					self.board.draw_board_tile(tile.label)
+					self.board.draw_board_tile(tile.label, colors['gry_tile'], (0,0,0))
 					new_tile = self.player.place_tile(tile, self.pile_tiles)
 					print(f"{len(self.pile_tiles)} Tiles Remaining: ", self.pile_tiles)
-					#print(new_tile.label, new_tile.rect)
-					self.player.draw_player_tiles(self.board, self.background)
+					#pygame.display.update(new_tile.rect)
+					self.board.draw_player_tiles(self.player.my_tiles)
 					break
 
 
 class Board(object):
-	board_margin = board_x = board_y = 20
 	max_player_tiles = 6
 	board_tiles = {}
-	# board border
-	border = 4
+	board_y = margin = 20
 
-	def __init__(self, background):
+	def __init__(self, dimensions, background):
+		self.rows, self.cols = dimensions
 		self.background_surface = background
-		self.rows, self.cols = (9, 12)
 		self.window_rect = background.get_rect()
-		# set maximum board size
-		self.board_width = self.window_rect.width - self.board_margin*2
-		self.board_height = self.window_rect.height - self.board_margin*3
-		print(self.board_height)
-		# make all tiles the same size
-		self.tile_width = int(self.board_width / self.cols)
-		# divide by rows + 1 to leave room for the player tiles
-		self.tile_height = int(self.board_height / (self.rows+1))
-		if self.tile_height < self.tile_width:
-			self.tile_width = self.tile_height
-		else:
-			self.tile_height = self.tile_width
-		# scale up board by tile size
-		self.board_width = self.tile_width * self.cols
-		self.board_height = self.tile_height * self.rows
-		print(self.board_height)
-		self.board_x = center(self.window_rect.width, self.board_width)
-
+		self.set_board_size()
 		self.draw_board()
 
+	def set_board_size(self):
+		# set maximum board size
+		max_height = self.window_rect.height - self.margin*3
+		# divide by rows + 1 to leave room for the player tiles
+		self.tile_size = int(max_height / (self.rows+1))
+		# scale board by tile size
+		self.board_width = self.tile_size * self.cols
+		self.board_height = self.tile_size * self.rows
+		self.board_x = center(self.window_rect.width, self.board_width)
 
 	def draw_board(self):
-		#pygame.draw.rect(surface, (150,150,150),
-		#				(self.board_x-border, self.board_y-border,
-		#				self.board_width+border*2, self.board_height+border*2),
-		#				border*2)
-		self.board_surface = pygame.Surface((self.board_width+self.border*2,
-											self.board_height+self.border*2))
-		pygame.draw.rect(self.board_surface, (150,150,150),
-						self.board_surface.get_rect(), self.border*2)
-		# board spaces
-		x = self.border
-		y = self.border
+		# board tiles
+		x = self.board_x
+		y = self.board_y
+		ts = self.tile_size
 		for i in range(self.rows):
 			for j in range(self.cols):
-				rect = pygame.draw.rect(self.board_surface, (0,0,125),
-								(x,y,self.tile_width,self.tile_height), 1)
+				rect = pygame.Rect(x,y,ts,ts)
 				text = f"{j+1}{chr(i+65)}"
-				self.board_tiles[text] = rect
-				self.draw_tile_text(text, self.board_surface, x, y, (255,255,255))
-				x += self.tile_width
-			x = self.border
-			y += self.tile_height
-
-		# blit board to surface
-		self.board_surface.convert()
-		self.background_surface.blit(self.board_surface,
-									(self.board_x-self.border, self.board_y-self.border))
+				tile = Tile(text, rect, self.tile_size)
+				self.board_tiles[text] = tile
+				self.draw_board_tile(text, colors['blk_background'], (255,255,255))
+				self.draw_board_tile(text, colors['blu_outline'], (255,255,255), 1)
+				#tile.draw_tile(self.background_surface, (0,0,125), (255,255,255), 1)
+				x += ts
+			x = self.board_x
+			y += ts
+		# border
+		border = 3
+		pygame.draw.rect(self.background_surface, (150,150,150),
+						(self.board_x-border, self.board_y-border,
+						self.board_width+border*2, self.board_height+border*2),
+						border)
 		# tile rack
-		rack_width = (self.tile_width + self.board_margin) * self.max_player_tiles
-		self.rack_x = center(self.window_rect.width, rack_width)
-		self.rack_y = self.board_height + self.board_margin*2
-		self.tile_rack_surface = pygame.Surface((rack_width, self.tile_height))
-		#pygame.draw.rect(self.tile_rack_surface, (255,0,0),
-		#				self.tile_rack_surface.get_rect(), 2)
-		self.blit_tile_rack(self.background_surface)
+		self.rack_width = (ts + self.margin) * self.max_player_tiles
+		self.rack_x = center(self.window_rect.width, self.rack_width)
+		self.rack_y = self.board_height + self.margin*2
 
-	def blit_tile_rack(self, blit_surface):
-		self.tile_rack_surface.convert()
-		blit_surface.blit(self.tile_rack_surface, (self.rack_x,self.rack_y))
+	def draw_board_tile(self, label, tile_color, text_color, width = 0):
+		tile = self.board_tiles[label]
+		tile.draw_tile(self.background_surface, tile_color, text_color, width)
 
-	def draw_tile_text(self, text, blit_surface, x, y, rgb):
-		font_file = os.path.join(path, "TarzanaNarrow_Bold.ttf")
-		#print(font_file)
-		tile_font = pygame.font.Font(font_file, 30)
-		text_box = tile_font.render(text, True, rgb)
-		font_width, font_height = tile_font.size(text)
-		font_x = center(self.tile_width, font_width)
-		font_y = center(self.tile_height, font_height)
-		text_box.convert()
-		blit_surface.blit(text_box, (x+font_x, y+font_y))
+	def draw_player_tiles(self, tiles):
+		for tile in tiles.values():
+			tile.draw_tile(self.background_surface, colors['gry_tile'], (0,0,0))
 
-	def draw_board_tile(self, tile_label):
-		print(tile_label, self.board_tiles[tile_label])
-		rect = self.board_tiles[tile_label]
-		pygame.draw.rect(self.board_surface, (125,125,125), rect)
-		text = f"{tile_label}"
-		self.draw_tile_text(text, self.board_surface, rect.left, rect.top, (0,0,0))
-		self.blit_board_tile(self.background_surface)
+	def highlight_tile(self, label):
+		tile = self.board_tiles[label]
+		tile.draw_tile(self.background_surface, (255, 255, 200), (0,0,0), 0, 200)
 
-	def blit_board_tile(self, blit_surface):
-		self.board_surface.convert()
-		blit_surface.blit(self.board_surface,
-							(self.board_x-self.border, self.board_y-self.border))
+	def unhighlight_tile(self, label):
+		tile = self.board_tiles[label]
+		tile.surface.fill(colors['blk_background'])
+		self.draw_board_tile(label, colors['blu_outline'], (255,255,255), 1)
 
 class Player(object):
-	my_tiles = []
-	tile_margin = 20
+	my_tiles = {}
+	margin = 20
 	def __init__(self, max_tiles):
 		self.max_tiles = max_tiles
 
 	def place_tile(self, placed_tile, new_tiles):
+		# new_tiles is a list
 		print("Placed Tile: ",placed_tile.label)
-		for i, tile in enumerate(self.my_tiles):
-			if tile.label == placed_tile.label:
+		for label,tile in self.my_tiles.items():
+			if label == placed_tile.label:
+				self.my_tiles.pop(label)
 				if len(new_tiles) > 0:
 					choice = random.choice(new_tiles)
 					loc = new_tiles.index(choice)
-					self.my_tiles[i] = Tile(new_tiles.pop(loc), tile.rect)
-					return self.my_tiles[i]
+					self.my_tiles[choice] = Tile(new_tiles.pop(loc),
+												tile.rect, tile.size)
+					return self.my_tiles[choice]
 				else:
-					self.my_tiles.pop(i)
 					return None
 
-
-	def select_tiles(self, new_tiles):
-		self.my_tiles = []
+	def starting_tiles(self, new_tiles, rack_x, rack_y, tile_size):
+		# new_tiles is a list
+		x = rack_x + int(self.margin/2)
 		while len(self.my_tiles) < self.max_tiles:
-			if len(new_tiles) == 0:
-				break
 			choice = random.choice(new_tiles)
 			loc = new_tiles.index(choice)
-			self.my_tiles.append(Tile(new_tiles.pop(loc)))
-		#print("Player Tiles:", self.my_tiles)
-		return new_tiles, self.my_tiles
+			rect = pygame.Rect(x, rack_y, tile_size, tile_size)
+			self.my_tiles[choice] = Tile(new_tiles.pop(loc), rect, tile_size)
+			x += (tile_size + self.margin)
 
 	def discard_tile(self, discarded_tile, new_tiles):
+		# new_tiles is a list
 		print("Discarded Tile: ",discarded_tile.label)
 		for i, tile in enumerate(self.my_tiles):
 			if tile.label == discarded_tile.label:
 				choice = random.choice(new_tiles)
 				loc = new_tiles.index(choice)
-				self.my_tiles[i] = Tile(new_tiles.pop(loc), tile.rect)
+				self.my_tiles[i] = Tile(new_tiles.pop(loc), tile.rect, tile.size)
 				return self.my_tiles[i]
 
-	"""def draw_player_tile(self, tile, board, blit_surface):
-		pygame.draw.rect(board.tile_rack_surface, (125,125,125),
-						tile.rect)
-		text = f"{tile.label}"
-		board.draw_tile_text(text, board.tile_rack_surface,
-							tile.rect.left, tile.rect.top, (0,0,0))
-		board.blit_tile_rack(blit_surface)"""
-
-	def draw_player_tiles(self, board, blit_surface):
-		x = int(self.tile_margin/2)
-		y = 0
-		board.tile_rack_surface.fill((0,0,0))
-		for tile in self.my_tiles:
-			rect = pygame.draw.rect(board.tile_rack_surface, (125,125,125),
-							(x, y, board.tile_width, board.tile_height))
-			tile.rect = rect.move(board.rack_x, board.rack_y)
-			text = f"{tile.label}"
-			board.draw_tile_text(text, board.tile_rack_surface, x, y, (0,0,0))
-			x += (board.tile_width + self.tile_margin)
-		board.blit_tile_rack(blit_surface)
-
 class Tile(object):
-	def __init__(self, label, rect = (0, 0, 50, 50)):
+	hover = False
+	def __init__(self, label, rect, size):
 		self.label = label
 		self.rect = rect
+		self.size = size
+		self.surface = pygame.Surface((size,size))
+
+	def draw_tile(self, blit_surface, tile_color, text_color, width = 0, alpha = None):
+		pygame.draw.rect(self.surface, tile_color, (0,0,self.size,self.size), width)
+		self.surface.set_alpha(alpha)
+		self.draw_text(text_color)
+		#self.surface = pygame.Surface.convert_alpha(self.surface)
+		blit_surface.blit(self.surface, self.rect)
+
+	def draw_text(self, rgb):
+		tile_font = pygame.font.Font(font_file, 30)
+		text_box = tile_font.render(self.label, True, rgb)
+		font_width, font_height = tile_font.size(self.label)
+		font_x = center(self.size, font_width)
+		font_y = center(self.size, font_height)
+		text_box.convert()
+		self.surface.blit(text_box, (font_x, font_y))
 
 
 if __name__ == '__main__':
